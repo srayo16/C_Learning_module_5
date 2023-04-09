7a2bfade600dd22c3a3ff5746d675006048aea7b
srayo16 <srayosikder@gmail.com>
2023-04-09 07:03:43 +0600

    six and last

--- fifth_prob.c
+++ fifth_prob.c
@@ -1,3 +1,13 @@
+// Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.
+
+// Note:
+
+// Digits in ASCII '0' = 48,'1' = 49 ....etc
+// Capital letters in ASCII 'A' = 65, 'B' = 66 ....etc
+// Small letters in ASCII 'a' = 97,'b' = 98 ....etc
+// Input
+// Only one line containing a character X which will be a capital or small letter or digit.
+
 #include <stdio.h>
 
 int main()
--- fourth_prob.c
+++ fourth_prob.c
@@ -1,3 +1,13 @@
+// Given a number X. Print "EVEN" if the first digit of X is even number. Otherwise print "ODD".
+
+// For example: In 4569 the first digit is 4, the second digit is 5, the third digit is 6 and the fourth digit is 9.
+
+// Input
+// Only one line containing a number X (999 < X  ≤  9999)
+
+// Output
+// If the first digit is even print "EVEN" otherwise print "ODD".
+
 #include <stdio.h>
 
 int main()
