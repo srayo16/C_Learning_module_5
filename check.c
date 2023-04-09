e234d0059d26b1f0d9a72a0b4f64c1a7d22d5015
srayo16 <srayosikder@gmail.com>
2023-04-09 06:01:07 +0600

    first problem

--- check.c
+++ check.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    if ((a && b) >= 0 && (a && b) <= 100)
+    {
+        if (a >= b)
+        {
+            printf("Yes");
+        }
+        else
+        {
+            printf("No");
+        }
+    }
 
     return 0;
 }
