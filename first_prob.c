7e77d8eb7bf775b0fdc1bebd12556a73b810bd22
srayo16 <srayosikder@gmail.com>
2023-04-09 06:08:38 +0600

    second one

--- first_prob.c
+++ first_prob.c
@@ -1,3 +1,11 @@
+// Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+
+// Input
+// Only one line containing two numbers A and B (0  ≤  A, B  ≤  100).
+
+// Output
+// Print "Yes" or "No" according to the statement.
+
 #include <stdio.h>
 
 int main()
